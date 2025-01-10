01b89c6508179bfdc7f2b8f4321dd1067c5d6e2e
Dexaahue <ajitb1687@gmail.com>
2025-01-10 12:37:21 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
+api_id = 20891424
+api_hash = "f29cef7feff53b2d9321c6b9e5d69b21"
+bot_token = "7935548546:AAEQpc5aCgzmP6xBiaVCEBCtOs0AfW-iB5k"
 auth_users = [1713924419]
 sudo_user = 1713924419
 log_channel = -1002143629860
